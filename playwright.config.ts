075a75a3be5eac18d13d9f28f1d0cc43ac708f9d
zcf0508 <zcf0508@live.com>
2024-02-18 11:57:42 +0800

    fix: e2e test fail

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,5 +3,9 @@ import { PlaywrightTestConfig } from '@playwright/test';
 const config: PlaywrightTestConfig = {
   // Look for test files in the "tests" directory, relative to this configuration file
   testDir: 'tests/e2e',
+  webServer: {
+    command: 'npm run build && npm run serve',
+    port: 4173,
+  },
 };
 export default config;
--- tests/e2e/example.spec.ts
+++ tests/e2e/example.spec.ts
@@ -1,8 +1,11 @@
 import { test, expect } from '@playwright/test';
 
 test('basic test', async ({ page }) => {
-  await page.goto('http://localhost:3000/');
+  await page.goto('http://127.0.0.1:4173', {
+    waitUntil: 'load',
+  });
   const menus = await page.locator('a span').allTextContents();
+  console.log(menus)
   expect(menus[0]).toBe('Go to Home');
   expect(menus[1]).toBe('Go to About');
 });
