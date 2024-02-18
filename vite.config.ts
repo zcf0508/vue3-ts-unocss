f200d6bd5a2bb80a86fed1dc848cd702d8261624
zcf0508 <zcf0508@live.com>
2024-02-18 10:32:54 +0800

    feat: add `ts-reset`

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import * as path from 'path';
-import { PluginOption, defineConfig } from 'vite';
+import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 // import legacy from "@vitejs/plugin-legacy";
 import viteCompression from 'vite-plugin-compression'; //gzip
@@ -11,7 +11,7 @@ import Inspect from 'vite-plugin-inspect';
 import { CodeInspectorPlugin } from 'code-inspector-plugin';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command, mode }) => {
+export default defineConfig(({ command }) => {
 
   return {
     define: {
